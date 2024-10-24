3c8d86b5b1226b94947385ff596790590ea19eee
Simo Veijola <simo.veijola@aalto.fi>
2024-10-24 20:26:21 +0300

    update core.c

--- Ex3/core.c
+++ Ex3/core.c
@@ -74,7 +74,7 @@ void evolve_interior(field *curr, field *prev, double a, double dt)
     dy2 = prev->dy * prev->dy;
     // four columns at a time and three rows
     int nyv = (ny-2)/4;
-    int nxv = (nx-2)/3;
+    int nxv = (nx-2)/2;
 
     // add the vectorized step for speedup
     // 21 vectors (icnluding the shuffle vectors) try to alter smaller
@@ -85,11 +85,11 @@ void evolve_interior(field *curr, field *prev, double a, double dt)
     const v4d invdx2 = _mm256_set1_pd(1/dx2);
     const v4d invdy2 = _mm256_set1_pd(1/dy2);
     // results and current rows
-    v4d res[3];
-    v4d vec[5];
+    v4d res[2];
+    v4d vec[4];
     //shifted vectors sl: shift left, sr: shift right
-    v4d vec_sl[3];
-    v4d vec_sr[3];
+    v4d vec_sl[2];
+    v4d vec_sr[2];
 
     // holders for values on the right and leftside of current vector
     double leftside[3], rightside[3];
@@ -98,27 +98,21 @@ void evolve_interior(field *curr, field *prev, double a, double dt)
 
         for (j = 2; j < nyv*4+2; j+=4) {
 
-            #pragma unroll
-            for(int row = 0; row < 3; ++row) {
+
+           #pragma unroll
+            for(int row = 0; row < 2; ++row) {
                 leftside[row] = prev -> data[idx(i+row, j-1, width)];
-                rightside[row] = prev -> data[idx(i+row, j+4, width)];
+                rightside[row] = prev -> data[idx(i+row, j+3, width)];
             }
-
-            // PREVIOUS AND NEXT ROW ABOVE AND BELOW THESE THREE
-            vec[0] = _mm256_loadu_pd(prev -> data + idx(i-1, j, width));
-            vec[4] = _mm256_loadu_pd(prev -> data + idx(i+3, j, width));
-
-            // THE IDEA: these operations should be automatically such that the processor pipelines
-            // these operations for the three rows and performs them out of order
-
+            
             #pragma unroll
-            for(int row = 0; row < 3; ++row) {
+            for(int row = 0; row < 2; ++row) {
                 // load the vector from memory
                 vec[row+1] = _mm256_loadu_pd(prev -> data + idx(i+row, j, width));
             }
 
             #pragma unroll
-            for(int row = 0; row < 3; ++row) {
+            for(int row = 0; row < 2; ++row) {
                 // shift the vectors left
                 vec_sl[row] = __builtin_shuffle(vec[row+1], vecz, shuffle1);
                 // shift the vectors right
@@ -139,6 +133,7 @@ void evolve_interior(field *curr, field *prev, double a, double dt)
                 // store the final vectors into the memory
                 _mm256_storeu_pd(curr -> data + idx(i+row, j, width), res[row]);
             }
+            
         }
     }
 
@@ -238,6 +233,65 @@ void evolve_edges(field *curr, field *prev, double a, double dt)
     }
 }
 
+/*
+            #pragma unroll
+            for(int row = 0; row < 3; ++row) {
+                leftside[row] = prev -> data[idx(i+row, j-1, width)];
+                rightside[row] = prev -> data[idx(i+row, j+4, width)];
+            }
+
+            // PREVIOUS AND NEXT ROW ABOVE AND BELOW THESE THREE
+            vec[0] = _mm256_loadu_pd(prev -> data + idx(i-1, j, width));
+            vec[4] = _mm256_loadu_pd(prev -> data + idx(i+3, j, width));
+
+            // THE IDEA: these operations should be automatically such that the processor pipelines
+            // these operations for the three rows and performs them out of order
+
+            vec[1] = _mm256_loadu_pd(prev -> data + idx(i, j, width));
+            vec[2] = _mm256_loadu_pd(prev -> data + idx(i+1, j, width));
+            vec[3] = _mm256_loadu_pd(prev -> data + idx(i+2, j, width));
+
+            vec_sl[0] = __builtin_shuffle(vec[1], vecz, shuffle1);
+            vec_sl[1] = __builtin_shuffle(vec[2], vecz, shuffle1);
+            vec_sl[2] = __builtin_shuffle(vec[3], vecz, shuffle1);
+            // shift the vectors right
+            vec_sr[0] = __builtin_shuffle(vecz, vec[1], shuffle2);
+            vec_sr[1] = __builtin_shuffle(vecz, vec[2], shuffle2);
+            vec_sr[2] = __builtin_shuffle(vecz, vec[3], shuffle2);
+            // add the right side and left side values together
+            vec_sl[0] += vec_sr[0];
+            vec_sl[1] += vec_sr[1];
+            vec_sl[2] += vec_sr[2];
+            // multiply center value with the coefficient and add the previous row at the same time
+            res[0] = _mm256_fmadd_pd(coeff, vec[1], vec[0]);
+            res[1] = _mm256_fmadd_pd(coeff, vec[2], vec[1]);
+            res[2] = _mm256_fmadd_pd(coeff, vec[3], vec[2]);
+            vec_sl[0] = _mm256_fmadd_pd(coeff, vec[1], vec_sl[0]);
+            vec_sl[1] = _mm256_fmadd_pd(coeff, vec[2], vec_sl[1]);
+            vec_sl[2] = _mm256_fmadd_pd(coeff, vec[3], vec_sl[2]);
+            vec_sl[0][0] += leftside[0];
+            vec_sl[1][0] += leftside[1];
+            vec_sl[2][0] += leftside[2];
+            vec_sl[0][3] += rightside[0];
+            vec_sl[1][3] += rightside[1];
+            vec_sl[2][3] += rightside[2];
+            // add the row below to this row and multiply by the 1/dx2
+            res[0] = invdx2*(res[0] + vec[2]);
+            res[1] = invdx2*(res[1] + vec[3]);
+            res[2] = invdx2*(res[2] + vec[4]);
+            // add the two scaled terms together and store in res
+            res[0] = _mm256_fmadd_pd(invdy2, vec_sl[0], res[0]);
+            res[1] = _mm256_fmadd_pd(invdy2, vec_sl[1], res[1]);
+            res[2] = _mm256_fmadd_pd(invdy2, vec_sl[2], res[2]);
+            // once more use fmadd to multiply res with a*dt and add previous vector
+            res[0] = _mm256_fmadd_pd(adt, res[0], vec[1]);
+            res[1] = _mm256_fmadd_pd(adt, res[1], vec[2]);
+            res[2] = _mm256_fmadd_pd(adt, res[2], vec[3]);
+            // store the final vectors into the memory
+            _mm256_storeu_pd(curr -> data + idx(i, j, width), res[0]);
+            _mm256_storeu_pd(curr -> data + idx(i+1, j, width), res[1]);
+            _mm256_storeu_pd(curr -> data + idx(i+2, j, width), res[2]);
+*/
 
 /*
             prev_row = _mm256_load_pd(prev->data[idx(i-1, j, width)]);
